6943e7780ca569d6e711ab28fe84bf05cf26b5c7
Ubuntu <ubuntu@ip-172-31-9-10.ec2.internal>
2021-02-27 03:10:57 +0000

    Creacion de migracion para nuestras tablas

---
+++ database/migrations/2021_02_26_222355_create_disco_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('Nombre_cancion');
+            $table->string('Nombre_album');
+            $table->string('Nombre_discos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
