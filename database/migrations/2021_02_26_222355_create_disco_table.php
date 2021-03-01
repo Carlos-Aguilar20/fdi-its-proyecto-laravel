82b2da9b90831a05af06f8b88093c1d9a622e25b
Carlos-Aguilar20 <71714915+Carlos-Aguilar20@users.noreply.github.com>
2021-02-28 19:34:24 -0600

    Update 2021_02_26_222355_create_disco_table.php

--- database/migrations/2021_02_26_222355_create_disco_table.php
+++ database/migrations/2021_02_26_222355_create_disco_table.php
@@ -13,7 +13,7 @@ class CreateDiscoTable extends Migration
      */
     public function up()
     {
-        Schema::create('Disco', function (Blueprint $table) {
+        Schema::create('disco', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('Nombre_cancion');
